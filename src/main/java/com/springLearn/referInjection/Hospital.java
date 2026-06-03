06ca0ff09bbfde2788575ad2191234fcdc77122f
Kuldeep Kumar Kashyap <kuldeepkashyap0206@gmail.com>
2026-06-04 00:36:05 +0530

    Learning Ref injection in the Spring Bean injection

--- src/main/java/com/springLearn/referInjection/Hospital.java
+++ src/main/java/com/springLearn/referInjection/Hospital.java
@@ -1,11 +1,19 @@
 package com.springLearn.referInjection;
 
+import lombok.*;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@AllArgsConstructor
 public class Hospital {
 
     //hospital Name;
 
     private String hospitalName;
-    //private  Doctor  doctor;
+    private  Doctor  doctor;
 
 
 }
