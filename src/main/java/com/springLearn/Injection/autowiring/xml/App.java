eced8b7245cd4858570cbfbd7515896224007c0f
Kuldeep Kumar Kashyap <kuldeepkashyap0206@gmail.com>
2026-06-05 00:47:35 +0530

    Autowiring 1st xml method

---
+++ src/main/java/com/springLearn/Injection/autowiring/xml/App.java
@@ -0,0 +1,20 @@
+package com.springLearn.Injection.autowiring.xml;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("autowiringxml.xml");
+
+        Student student= context.getBean("student",Student.class);
+
+        student.study();
+
+        //output
+        //Laptop complie code
+        //Student study
+
+    }
+}
---
+++ src/main/java/com/springLearn/Injection/autowiring/xml/Laptop.java
@@ -0,0 +1,7 @@
+package com.springLearn.Injection.autowiring.xml;
+
+public class Laptop {
+    public void complieCode(){
+        System.out.println("Laptop complie code");
+    }
+}
