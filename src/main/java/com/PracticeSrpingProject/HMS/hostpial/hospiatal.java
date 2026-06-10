017ae36d7e486cd9a22cde3118b1057f64f2d76b
Kuldeep Kumar Kashyap <kuldeepkashyap0206@gmail.com>
2026-06-10 23:50:34 +0530

    Added  Practice Project

---
+++ src/main/java/com/PracticeSrpingProject/HMS/hostpial/hospiatal.java
@@ -0,0 +1,9 @@
+package com.PracticeSrpingProject.HMS.hostpial;
+
+import com.springLearn.Injection.referInjection.Doctor;
+
+public class hospiatal {
+
+    private String hospital;
+    private Doctor doctor;
+}
--- src/main/java/com/springLearn/Injection/referInjection/Doctor.java
+++ src/main/java/com/springLearn/Injection/referInjection/Doctor.java
@@ -1,19 +1,19 @@
-package com.springLearn.Injection.referInjection;
-
-import lombok.*;
-
-@Data
-@Setter
-@Getter
-@AllArgsConstructor
-
-@NoArgsConstructor
-@ToString
-public class Doctor {
-    private String doctorName;
-    private String doctorEmail;
-    private String doctorPhone;
-    private int doctorId;
-
-
-}
+//package com.springLearn.Injection.referInjection;
+//
+//import lombok.*;
+//
+//@Data
+//@Setter
+//@Getter
+//@AllArgsConstructor
+//
+//@NoArgsConstructor
+//@ToString
+//public class Doctor {
+//    private String doctorName;
+//    private String doctorEmail;
+//    private String doctorPhone;
+//    private int doctorId;
+//
+//
+//}
